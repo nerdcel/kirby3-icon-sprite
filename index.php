0e499b14d2da271b2e98e96a3f91bf833b41aa0c
Marcel Hieke <office@marcelhieke.com>
2023-02-13 12:11:58 +0100

    add tabs blueprint

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ Kirby::plugin('nerdcel/icon-sprite', [
         'blocks/icon' => __DIR__ . '/blueprints/blocks/icon.yml',
         'files/svgicon' => __DIR__ . '/blueprints/files/svgicon.yml',
         'sections/svgsprite' => __DIR__ . '/blueprints/sections/svgsprite.yml',
+        'tabs/icons' => __DIR__ . '/blueprints/tabs/icons.yml',
     ],
 
     'api' => [
